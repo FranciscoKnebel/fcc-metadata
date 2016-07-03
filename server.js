5283648ae2554360aecc885e5eaec30672d8089a
FPK <emaildabisnaga@gmail.com>
2016-07-03 02:59:40 +0000

    Added file uploadinad, parse and response.

--- server.js
+++ server.js
@@ -12,7 +12,31 @@ app.get('/LICENSE', function (req, res) {
   res.sendFile(__dirname + 'LICENSE.md');
 })
 
+
+/* File handling */
+var upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('file'),  function(req, res, next) {
+  console.log(req.file);
+  var fileInfo = parseNewFile(req.file);
+  
+  res.contentType('application/json');
+  res.send(fileInfo);
+});
+
+
 var port = process.env.PORT || 80;
 app.listen(port, function(){
   console.log('Listening on port ' + port + '...');
 });
+
+
+function parseNewFile(file) {
+  var obj = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size + " bytes"
+  }
+  
+  return obj;
+}
